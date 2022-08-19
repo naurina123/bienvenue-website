d96c6556d9c570f6075ddae204ad1de7336b8e0a
naurina123 <naurina.orio@gmail.com>
2022-08-19 15:34:58 +0500

    animation library added

--- components/Features.js
+++ components/Features.js
@@ -1,3 +1,5 @@
+import Fade from 'react-reveal/Fade';
+
 const Features = ({ image, background, heading, body }) => {
   return (
     <div className="">
@@ -7,17 +9,16 @@ const Features = ({ image, background, heading, body }) => {
             className=" lg:w-[30rem] lg:h-[30rem] lg:rounded-full w-[20rem] h-[20rem] rounded-full"
             style={{ background: background }}
           ></div>
-          <img
-            src={image}
-            className="w-[10rem] lg:w-[20rem] absolute transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-          />
+          <img src={image} className="w-[10rem] lg:w-[20rem] absolute transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
         </div>
-        <div className="flex-1">
-          <h2 className="features-heading w-[18rem] lg:w-[40rem]  mb-[2rem]">
+       <Fade right duration={1900}> <div className="flex-1">
+        
+          <h2 className="features-heading w-[18rem] lg:w-[40rem]  mb-[2rem] ">
             {heading}
           </h2>
           {body}
         </div>
+        </Fade>
       </div>
     </div>
   );
--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,17 @@
+import Fade from 'react-reveal/Fade';
 const Hero = () => {
   return (
     <div className="primary-bg hero-bg">
       <div className="min-h-[100vh] flex flex-col lg:flex-row items-center justify-center mx-auto lg:container p-[3rem] gap-[2rem] lg:gap-[5rem]">
-        <div className="flex-1 text-white">
+       <Fade left duration={1500}><div className="flex-1 text-white">
           <h1>Spend Less, Do More</h1>
           <p className="body-1 mt-[2rem]">
             Bienvenue brings you buy one get one & discount offers for
             restaurants and entertainment facilities across Canada.
           </p>
         </div>
-        <div className="flex-1 flex justify-center items-center">
+        </Fade> 
+        <div className="flex-1 flex justify-center items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
           <img src="images/hero.svg" className="w-[35rem]" />
         </div>
       </div>
