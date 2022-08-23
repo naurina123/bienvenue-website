4cf2c62f18fa13d624c17138676ce856223ab333
naurina123 <naurina.orio@gmail.com>
2022-08-23 13:53:10 +0500

    cosmetic changes done

--- components/Header.js
+++ components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
     >
       <img
         src={small ? "images/Bienvenue.png" : "images/logo-header.svg"}
-        className={` my-[1rem] mx-[2.75rem] w-[10rem]  lg:my-[2rem] lg:mx-[4.25rem] ${
+        className={` my-[1rem] mx-[1rem]  w-[10rem]  lg:my-[2rem] lg:mx-[4.25rem] ${
           small ? "h-[2rem] object-contain" : "h-[5rem] "
         }`}
       />
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,21 @@
 import Link from "next/link";
 const Navbar = () => {
-    return ( 
-        <div>
-            <ul className="flex ">
-                <li className="mr-6">
-                <Link href="/"><a className="text-black hover:text-black" >Home-1</a></Link> 
-                </li>
-                <li className="mr-6">
-                   <Link href="/home-2"><a className="text-black hover:text-black" >Home-2</a></Link> 
-                </li>
-                
-            </ul>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <ul className="hidden lg:flex">
+        <li className="mr-6">
+          <Link href="/">
+            <a className="text-black hover:text-black">Home-1</a>
+          </Link>
+        </li>
+        <li className="mr-6">
+          <Link href="/home-2">
+            <a className="text-black hover:text-black">Home-2</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default Navbar;
