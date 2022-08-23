57d5045b11de1da6c8dd278e3a6b99fbc8918f6e
naurina123 <naurina.orio@gmail.com>
2022-08-23 13:58:21 +0500

    responsive changes

--- components/Footer.js
+++ components/Footer.js
@@ -9,15 +9,31 @@ const Footer = () => {
             restaurants and entertainment facilities across Canada.
           </p>
           <div className="flex gap-[1rem] ">
-            <a href="#" target="_blank"><img src="images/twitter.svg" className="w-[2rem]" /></a>
-            <a href="https://www.facebook.com/ORIO-103986585601560/" target="_blank"><img src="images/facebook.svg" className=" w-[2rem]" /></a>
-            <a href="https://www.linkedin.com/company/orio.tech/" target="_blank"><img src="images/linkedin.svg" className=" w-[2rem]" /></a>
+            <a href="#" target="_blank">
+              <img src="images/twitter.svg" className="w-[2rem]" />
+            </a>
+            <a
+              href="https://www.facebook.com/ORIO-103986585601560/"
+              target="_blank"
+            >
+              <img src="images/facebook.svg" className=" w-[2rem]" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/orio.tech/"
+              target="_blank"
+            >
+              <img src="images/linkedin.svg" className=" w-[2rem]" />
+            </a>
           </div>
         </div>
         <div className="flex flex-1 flex-col justify-center gap-[1rem]">
           <h5 className="">Contact</h5>
-          <p><a href="mailto:bienvenueco.com">bienvenueco.com</a></p>
-          <p><a href="tel:+14164525630">+1 (416) 452 - 5630</a></p>
+          <p>
+            <a href="mailto:bienvenueco.com">bienvenueco.com</a>
+          </p>
+          <p>
+            <a href="tel:+14164525630">+1 (416) 452 - 5630</a>
+          </p>
           <p>
             1609 - 16 Harrison Garden Blvd, North York, Toronto, On, M2N 7J6
           </p>
@@ -31,7 +47,7 @@ const Footer = () => {
             <input
               type="text"
               placeholder="Email Address"
-              className="rounded-l-md p-[1rem]  bg-[#323C41] h-[4rem] "
+              className="rounded-l-md p-[1rem]  bg-[#323C41] h-[4rem] w-[10rem] lg:w-[15rem] "
             />
             <button className="bg-black rounded-r-md w-[100px] h-[4rem] lg:text-[1rem] text-[.75rem] hover:shadow-md hover:bg-[#fcc53d] hover:transition hover:ease-in">
               Submit
@@ -40,7 +56,10 @@ const Footer = () => {
         </div>
       </div>
       <div className="border-t text-center p-[2rem]">
-        <p>Copyright 2022.<a href="https://uat.bienvenueco.com/" >Bienvenue.</a> All Rights Reserved</p>
+        <p>
+          Copyright 2022.<a href="https://uat.bienvenueco.com/">Bienvenue.</a>{" "}
+          All Rights Reserved
+        </p>
       </div>
     </div>
   );
